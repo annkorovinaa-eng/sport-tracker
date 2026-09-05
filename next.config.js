6294eeee59d9cad1a48efaf91bf41724bd48a3c6
annkorovinaa-eng <annkorovinaa@gmail.com>
2026-09-06 00:19:36 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/wallpaper': ['./assets/fonts/**'],
+    },
+  },
+};
 
 module.exports = nextConfig;
